0f7414f8d8840e60a27761c3b786c4c47cd4e4c8
Charlie Boggus <charlie.boggus@gmail.com>
2022-11-16 16:51:03 -0500

    resolve merge conflicts on branch

--- src/listeners/message-listener.ts
+++ src/listeners/message-listener.ts
@@ -2,11 +2,23 @@ import { Client } from 'discord.js'
 import { Logger } from '../util/logger'
 import { Listener } from './listener'
 
+let messageBatch: string[] = []
+
 class MessageListener implements Listener {
     public attachClient (client: Client): void {
         client.on('messageCreate', async (message) => {
             Logger.log(`Received message: ${message.content}`)
             // we want to do shit with recording  messages here... call the firehose? save to s3? we will figure it out
+            if (messageBatch.length > 5) {
+                messageBatch.forEach(message => {
+                })
+                // call the lambda here
+                messageBatch = []
+            }
+            else {
+                messageBatch.push(message.content)
+                Logger.log(`Batched message: ${message.content}`)
+            }
         })
     }
 }
