20d3d2231ab868199ee4ab3b5830f13c5c744548
Charlie Boggus <charlie.boggus@gmail.com>
2022-11-15 19:03:04 -0500

    added listeners and other boilerplate shit

---
+++ src/listeners/message-listener.ts
@@ -0,0 +1,12 @@
+import { Client } from 'discord.js'
+import { Listener } from '../interfaces/listener'
+
+class MessageListener implements Listener {
+    public start (client: Client): void {
+        client.on('messageCreate', async (message) => {
+            console.log(message.content)
+            // we want to do shit with recording  messages here... call the firehose? save to s3? we will figure it out
+        })
+    }
+}
+export const messageListener = new MessageListener()
