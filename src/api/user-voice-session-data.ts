73e048a7e433f9ed82d4401343206f10577dc92e
Charlie Boggus <charlie.boggus@gmail.com>
2023-03-08 22:48:32 -0500

    initial implementation of the voice chat listener

---
+++ src/api/user-voice-session-data.ts
@@ -0,0 +1,7 @@
+
+export interface UserVoiceSessionData {
+    joinedTimestamp: number
+    leaveTimestamp: number
+    userId?: string
+    userDisplayName: string
+}
--- src/listeners/voice-channel-listener.ts
+++ src/listeners/voice-channel-listener.ts
@@ -1,25 +1,69 @@
 import { Client } from "discord.js";
+import { UserVoiceSessionData } from "../api/user-voice-session-data";
 import { AppState } from "../app";
+import { S3Client } from "../clients/s3-client";
+import { Logger } from "../util/logger";
 import { Listener } from "./listener";
-
 
 export class VoiceChannelListener implements Listener {
 
-    // the current number of users in voice chat -- when this reaches 0 the voice session resets
     private userCount: number = 0
-
-    // tracks how much time each user spends in a voice chat session
-    private voiceSessionMap: Map<string, number> = new Map()
+    private voiceSession: Map<string, UserVoiceSessionData> = new Map()
 
     public attachClient(client: Client, appState: AppState): void {
         if (appState.shouldListen) {
             client.on('voiceStateUpdate', async (oldState, newState) => {
+                // user joined voice chat
                 if (!oldState.channelId) {
-                    // user joined voice chat -- increment userCount, if userCount is 0, start a new session
+                    this.userCount++
+                    const user = newState.member
+                    const userDisplayName = user? user.displayName : ''
+                    if (user && !this.voiceSession.has(userDisplayName)) {
+                        const userVoiceSessionData: UserVoiceSessionData = {
+                            joinedTimestamp: Date.now(),
+                            leaveTimestamp: -1,
+                            userId: user.id,
+                            userDisplayName: user.displayName
+                        }
+                        this.voiceSession.set(userDisplayName, userVoiceSessionData)
+                    }
                 }
 
+                // user leaves voice chat
                 if (!newState.channelId) {
-                    // user left voice chat -- decrement userCount, if  userCount is 0 end session
+                    this.userCount--
+                    const user = oldState.member
+                    if (user) {
+                        const displayName: string = user.displayName
+                        const userVoiceSessionData: UserVoiceSessionData | undefined = this.voiceSession.get(displayName)
+                        if (userVoiceSessionData) {
+                            userVoiceSessionData.leaveTimestamp = Date.now()
+                        }
+                    }
+
+                    /**
+                     * 
+                     * TODO:
+                     *    - does this code even work?
+                     *    - need to make the voice chat data bucket in S3 and figure out any IAM permissions
+                     *    - need to figure out how to SCP or whatever protocol into the EC2 instance to upload ./dist
+                     * 
+                     */
+
+                    if (this.userCount === 0) {
+                        // send voice session data to S3
+                        const voiceSessionBatch: UserVoiceSessionData[] = []
+                        for (const userData of this.voiceSession.values()) {
+                            voiceSessionBatch.push(userData)
+                        }
+                        const payload: string = JSON.stringify(voiceSessionBatch)
+                        const s3Client: S3Client = new S3Client(process.env.VOICE_DATA_BUCKET ?? '')
+                        s3Client.putObject(payload)
+                        Logger.log(`Sent batch to Lambda function: ${payload}`)
+
+                        // clear once the voice session ends. we  don't need the session to linger in memory
+                        this.voiceSession = new Map()
+                    }
                 }
 
                 if (oldState.channelId && newState.channelId !== oldState.channelId) {
