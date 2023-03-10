935b551356f03866190eba4cc457109d283136d9
Charlie Boggus <charlie.boggus@gmail.com>
2023-03-10 12:18:00 -0500

    forgot parenthesis

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -6,14 +6,14 @@ export class Logger {
     public static log(message: string, notify: boolean = false): void {
         console.log(`[${Date.now().toLocaleString()}][LOG]: ${message}`)
         if (notify) {
-            snsClient.publish(message, `[${Date.now().toLocaleString}][LOG] Hagrid Has something to say`)
+            snsClient.publish(message, `[${Date.now().toLocaleString()}][LOG] Hagrid Has something to say`)
         }
     }
 
     public static error(message: string, notify: boolean = true): void {
         console.log(`[${Date.now().toLocaleString()}][ERROR]: ${message}`)
         if (notify) {
-            snsClient.publish(message, `[${Date.now().toLocaleString}][ERROR] Hagrid has gone deaf!`)
+            snsClient.publish(message, `[${Date.now().toLocaleString()}][ERROR] Hagrid has gone deaf!`)
         }
     }
 }
