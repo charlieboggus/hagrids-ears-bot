1a1b740a4e063fe80b3d5b91aea9bce8135a2184
Charlie Boggus <charlie.boggus@gmail.com>
2023-03-10 12:21:52 -0500

    yet another fix to how date is formatted in notifications

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,16 +4,16 @@ const snsClient: SNSClient = new SNSClient()
 export class Logger {
 
     public static log(message: string, notify: boolean = false): void {
-        console.log(`[${Date.now().toLocaleString()}][LOG]: ${message}`)
+        console.log(`[${Date.now().toString()}][LOG]: ${message}`)
         if (notify) {
-            snsClient.publish(message, `[${Date.now().toLocaleString()}][LOG] Hagrid Has something to say`)
+            snsClient.publish(message, `[${Date.now().toString()}][LOG] Hagrid Has something to say`)
         }
     }
 
     public static error(message: string, notify: boolean = true): void {
-        console.log(`[${Date.now().toLocaleString()}][ERROR]: ${message}`)
+        console.log(`[${Date.now().toString()}][ERROR]: ${message}`)
         if (notify) {
-            snsClient.publish(message, `[${Date.now().toLocaleString()}][ERROR] Hagrid has gone deaf!`)
+            snsClient.publish(message, `[${Date.now().toString()}][ERROR] Hagrid has gone deaf!`)
         }
     }
 }
